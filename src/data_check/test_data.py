8424c31c6febcbbf971f5e9f2d22f771d1ae333c
Björn M <bjornmauthe@gmx.net>
2022-01-12 13:51:27 +0100

    added docstring and best hyperparameters

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -62,8 +62,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
 
 def test_row_count(data: pd.DataFrame):
+    """
+    Test proper data row lenghts 
+    """
     assert 15000 < data.shape[0] < 1000000
 
 
 def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test if all price values are between the defined price ranges
+    """
     assert data['price'].between(min_price, max_price).all()
