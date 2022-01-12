159ed5c712f2a531c2b465220e5245eb9f07f410
Björn M <bjornmauthe@gmx.net>
2022-01-12 14:07:04 +0100

    Added longitud a& latitude data cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -29,6 +29,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    idx = df['longitude'].between(-74.25, -
+                                  73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Save cleaned data as csv")
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
